32c1a8abccb8cd9806d567cb0820798b9713a2be
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-06-26 10:42:45 +0600

    first commit

---
+++ components/Home/Home.js
@@ -0,0 +1,11 @@
+import Banner from "@/components/Home/Banner/Banner";
+import Department from "@/components/Home/Department/Department";
+
+const Home = () => {
+    return <>
+        <Banner />
+        <Department />
+    </>;
+};
+
+export default Home;
