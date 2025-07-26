56f76b1077ebff9ba97474b1a3c612ee1a65f056
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-07-26 12:36:28 +0600

    Office Contact Information

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,7 @@
 import BranchOffice from '@/components/ContactUs/BranchOffice/BranchOffice';
 import ContactForm from '@/components/ContactUs/ContactForm/ContactForm';
 import GoogleMap from '@/components/ContactUs/GoogleMap/GoogleMap';
+import OfficeContact from '@/components/ContactUs/OfficeContact/OfficeContact';
 import GlobalLocation from '@/components/GlobalLocation/GlobalLocation';
 import PageBanner from '@/components/PageBanner/PageBanner';
 
@@ -11,6 +12,7 @@ const Contact = () => {
         title='Reach Out to Us for Any Inquiries'
         subtitle='Contact Us'
       />
+      <OfficeContact />
       <ContactForm />
       <BranchOffice />
       <GlobalLocation />
