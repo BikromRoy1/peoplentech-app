54fbbb19999e10d8c0b0cb2f8927a4bc9efabe88
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-08-02 15:44:33 +0600

    about us

--- app/about/page.js
+++ app/about/page.js
@@ -2,11 +2,13 @@ import PageBanner from '@/components/PageBanner/PageBanner';
 import SisterConcerns from '@/components/SisterConcerns/SisterConcerns';
 import FounderMs from './FounderMs/FounderMs';
 import OurGoal from './OurGoal/OurGoal';
+import OurStory from './OurStory/OurStory';
 
 const About = () => {
   return (
     <div>
       <PageBanner title='Our Company' subtitle='About' />
+      <OurStory />
       <OurGoal />
       <FounderMs />
       <SisterConcerns />
