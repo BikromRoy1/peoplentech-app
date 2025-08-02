74705e9767afb6a9ccbc5d909f5e884e4be71f7f
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-08-02 12:27:48 +0600

    Our Goal

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,17 @@
+import PageBanner from '@/components/PageBanner/PageBanner';
+import SisterConcerns from '@/components/SisterConcerns/SisterConcerns';
+import FounderMs from './FounderMs/FounderMs';
+import OurGoal from './OurGoal/OurGoal';
+
 const About = () => {
-  return <div>about page</div>;
+  return (
+    <div>
+      <PageBanner title='Our Company' subtitle='About' />
+      <OurGoal />
+      <FounderMs />
+      <SisterConcerns />
+    </div>
+  );
 };
 
 export default About;
