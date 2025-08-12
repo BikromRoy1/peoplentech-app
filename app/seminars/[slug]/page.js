6cb367e5d2f7af40463a47356f48352f593cf2b3
BikromRoy1 <bikrom@peoplentech.com.bd>
2025-08-12 17:35:38 +0600

    seminar page

---
+++ app/seminars/[slug]/page.js
@@ -0,0 +1,320 @@
+import { FaRegHourglassHalf } from 'react-icons/fa6';
+import { FiUsers } from 'react-icons/fi';
+import { RiTimerLine } from 'react-icons/ri';
+
+import { IoMdArrowDroprightCircle } from 'react-icons/io';
+
+import './SeminarDetails.css';
+
+export const Zoom = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='1em'
+    height='1em'
+    preserveAspectRatio='xMidYMid'
+    viewBox='0 0 256 256'
+    {...props}
+  >
+    <defs>
+      <linearGradient
+        id='zoom__a'
+        x1='23.666%'
+        x2='76.334%'
+        y1='95.6118%'
+        y2='4.3882%'
+      >
+        <stop offset='.00006%' stopColor='#0845BF' />
+        <stop offset='19.11%' stopColor='#0950DE' />
+        <stop offset='38.23%' stopColor='#0B59F6' />
+        <stop offset='50%' stopColor='#0B5CFF' />
+        <stop offset='67.32%' stopColor='#0E5EFE' />
+        <stop offset='77.74%' stopColor='#1665FC' />
+        <stop offset='86.33%' stopColor='#246FF9' />
+        <stop offset='93.88%' stopColor='#387FF4' />
+        <stop offset='100%' stopColor='#4F90EE' />
+      </linearGradient>
+    </defs>
+    <path
+      fill='url(#zoom__a)'
+      d='M256 128c0 13.568-1.024 27.136-3.328 40.192-6.912 43.264-41.216 77.568-84.48 84.48C155.136 254.976 141.568 256 128 256c-13.568 0-27.136-1.024-40.192-3.328-43.264-6.912-77.568-41.216-84.48-84.48C1.024 155.136 0 141.568 0 128c0-13.568 1.024-27.136 3.328-40.192 6.912-43.264 41.216-77.568 84.48-84.48C100.864 1.024 114.432 0 128 0c13.568 0 27.136 1.024 40.192 3.328 43.264 6.912 77.568 41.216 84.48 84.48C254.976 100.864 256 114.432 256 128Z'
+    />
+    <path
+      fill='#FFF'
+      d='M204.032 207.872H75.008c-8.448 0-16.64-4.608-20.48-12.032-4.608-8.704-2.816-19.2 4.096-26.112l89.856-89.856H83.968c-17.664 0-32-14.336-32-32h118.784c8.448 0 16.64 4.608 20.48 12.032 4.608 8.704 2.816 19.2-4.096 26.112l-89.6 90.112h74.496c17.664 0 32 14.08 32 31.744Z'
+    />
+  </svg>
+);
+
+const SeminarDetails = () => {
+  return (
+    <section>
+      <section className='pb-[80px] pt-[80px] page-banner-bg bg-[#020617] relative'>
+        <div
+          className='absolute inset-0 z-0'
+          style={{
+            backgroundImage: `radial-gradient(circle 500px at 50% 300px, rgba(4,169,255,0.35), transparent)`,
+          }}
+        />
+        <div className='mx-auto px-4 sm:px-6 container lg:px-8 relative'>
+          <div className='grid grid-cols-1 gap-4 md:grid-cols-8 md:items-center md:gap-9'>
+            <div className='md:col-span-5'>
+              <div>
+                <h4 className='text-2xl lg:text-3xl font-semibold capitalize text-white'>
+                  Post Graduate Diploma in Cyber Security
+                </h4>
+                <p className='text-gray-300 mt-4 text-[17px] font-siliguri leading-7 font-medium'>
+                  অবজেক্টিভ শুধু রিল বানানো নয়, এই ওয়েবিনারে শিখবেন কীভাবে
+                  স্মার্ট কাট, ডায়নামিক ট্রানজিশন আর মোশন গ্রাফিক্স ব্যবহার করে
+                  আপনার রিলকে বানাবেন একদম ভাইরাল-রেডি। After Effects-এর
+                  প্রফেশনাল টুলস দিয়ে বিটে কাটিং, টেক্সট ট্র্যাকিং, মাস্কিং, আর
+                  স্ট্যাবিলাইজেশন সবই শেখা যাবে হাতে-কলমে। সোশ্যাল মিডিয়া ফোকাসড
+                  এক্সপোর্ট সেটিং থেকে শুরু করে কালার গ্রেডিং পর্যন্ত, এই সেশন
+                  আপনার এডিটিং গেম বদলে দিতে পারে। তাই দেরি না করে এখনই
+                  রেজিস্টার করুন, আর রিল এডিটিংয়ে নিয়ে আসুন প্রফেশনাল ফিনিশিং
+                  টাচ!
+                </p>
+                <div className='flex items-center gap-4 mt-6 flex-wrap'>
+                  <div className='flex items-center gap-2 rounded-full bg-[#1f2838] px-6 py-2'>
+                    <FiUsers className='text-primary' />
+                    <span className='text-white font-semibold capitalize text-[15px]'>
+                      486+ Enrolled Students
+                    </span>
+                  </div>
+                  <div className='flex items-center gap-2 rounded-full bg-[#1f2838] px-6 py-2'>
+                    <FaRegHourglassHalf className='text-primary' />
+                    <span className='text-white font-semibold text-[15px]'>
+                      10:00 PM
+                    </span>
+                  </div>
+                  <div className='flex items-center gap-2 rounded-full bg-[#1f2838] px-6 py-2'>
+                    <RiTimerLine className='text-primary' />
+
+                    <span className='text-white font-semibold capitalize text-[15px]'>
+                      30 july, 2025
+                    </span>
+                  </div>
+                  <div className='flex items-center gap-2 rounded-full bg-[#1f2838] px-6 py-2'>
+                    <Zoom />
+                    <span className='text-white font-semibold text-[15px]'>
+                      Online class (ZOOM)
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='md:col-span-3 text-white'>
+              <div className='border-[7px] border-[#f3f4f6] rounded-[15px] overflow-hidden bg-white'>
+                <img
+                  className='w-full rounded-[10px]'
+                  src='/image/seminars.webp'
+                  alt='image'
+                />
+                <div className='p-4'>
+                  <div class='grid grid-cols-4 gap-4'>
+                    <div class='glass-card px-4 py-2 rounded-xl text-center transform hover:-translate-y-2 transition-all'>
+                      <div class='text-2xl font-bold text-white'>14</div>
+                      <div class='text-white/70 text-sm'>Days</div>
+                    </div>
+                    <div class='glass-card px-4 py-2 rounded-xl text-center transform hover:-translate-y-2 transition-all'>
+                      <div class='text-2xl font-bold text-white'>08</div>
+                      <div class='text-white/70 text-sm'>Hours</div>
+                    </div>
+                    <div class='glass-card px-4 py-2 rounded-xl text-center transform hover:-translate-y-2 transition-all'>
+                      <div class='text-2xl font-bold text-white'>42</div>
+                      <div class='text-white/70 text-sm'>Minutes</div>
+                    </div>
+                    <div class='glass-card px-4 py-2 rounded-xl text-center transform hover:-translate-y-2 transition-all'>
+                      <div class='text-2xl font-bold text-white'>30</div>
+                      <div class='text-white/70 text-sm'>Seconds</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='pb-[60px] pt-[60px]'>
+        <div className='mx-auto px-4 sm:px-6 container lg:px-8'>
+          <div className='grid grid-cols-1 gap-4 md:grid-cols-8 md:gap-9'>
+            <div className='md:col-span-5'>
+              <div className=''>
+                <div className='border border-[#dee2e6] rounded-xl p-6'>
+                  <h2 className='mb-7 text-2xl font-semibold capitalize font-siliguri leading-[30px] text-[#003384]'>
+                    এই ফ্রি সেমিনারে যা যা শিখবেন-
+                  </h2>
+
+                  <div>
+                    <ul>
+                      <li className='pb-4'>
+                        <div className='flex items-center gap-2'>
+                          <div>
+                            <IoMdArrowDroprightCircle className='w-[22px] h-[22px] text-primary' />
+                          </div>
+                          <p className='text-base  font-siliguri'>
+                            ৫টি লাইভ মাইক্রো প্রজেক্ট ডেভেলপমেন্ট: লাইভ কোডিং
+                            সেশনের মাধ্যমে ৫টি ইন্টারেস্টিং মাইক্রো প্রজেক্ট
+                            ডেভেলপমেন্ট!
+                          </p>
+                        </div>
+                      </li>
+                      <li className='pb-4'>
+                        <div className='flex items-center gap-2'>
+                          <div>
+                            <IoMdArrowDroprightCircle className='w-[22px] h-[22px] text-primary' />
+                          </div>
+                          <p className='text-base  font-siliguri'>
+                            Build as You Learn প্রসেস: লার্নিংয়ের পাশাপাশি
+                            প্রজেক্ট তৈরি হবে ডিরেক্ট এক্সপেরিয়েন্সে!
+                          </p>
+                        </div>
+                      </li>
+                      <li className='pb-4'>
+                        <div className='flex items-center gap-2'>
+                          <div>
+                            <IoMdArrowDroprightCircle className='w-[22px] h-[22px] text-primary' />
+                          </div>
+                          <p className='text-base  font-siliguri'>
+                            স্টেপ-বাই-স্টেপ লাইভ কোডিং: প্রতিটি প্রজেক্ট স্টেপ
+                            বাই স্টেপ ডেভেলপ করে দেখিয়ে দেওয়া হবে, যাতে আপনি
+                            সহজেই ফলো করতে পারেন।
+                          </p>
+                        </div>
+                      </li>
+                      <li className='pb-4'>
+                        <div className='flex items-center gap-2'>
+                          <div>
+                            <IoMdArrowDroprightCircle className='w-[22px] h-[22px] text-primary' />
+                          </div>
+                          <p className='text-base  font-siliguri'>
+                            পাইথন প্রবলেম সলভিং ফ্রেমওয়ার্ক: কীভাবে একটি
+                            প্রবলেমকে স্টেপ বাই স্টেপ সল্ভ করবেন, তার
+                            স্ট্র্যাটেজি।
+                          </p>
+                        </div>
+                      </li>
+                      <li className=''>
+                        <div className='flex items-center gap-2'>
+                          <div>
+                            <IoMdArrowDroprightCircle className='w-[22px] h-[22px] text-primary' />
+                          </div>
+                          <p className='text-base  font-siliguri'>
+                            প্রজেক্ট স্ট্রাকচারিং ও ডিবাগিং গাইড: প্রজেক্টকে
+                            সঠিকভাবে সাজানো এবং কোডের এরোর খুঁজে বের করার উপায়।
+                          </p>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='md:col-span-3'>
+              <div>
+                <div className='border border-[#dee2e6] rounded-xl px-6 py-8'>
+                  <div className='mb-6'>
+                    <h1 className='text-2xl/tight mb-3 font-semibold font-siliguri capitalize'>
+                      সম্পূর্ণ ফ্রি-তে রেজিস্ট্রেশন করুন
+                    </h1>
+                  </div>
+                  <form className='space-y-5'>
+                    <div>
+                      <label
+                        htmlFor='name'
+                        className='font-medium text-sm block mb-2'
+                      >
+                        Full name
+                      </label>
+                      <input
+                        id='name'
+                        type='text'
+                        placeholder='Enter Your Name'
+                        className='text-gray-500 border border-gray-300 focus:ring-0 focus:outline-none focus:border-primary text-sm rounded-lg py-2.5 px-4 w-full'
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor='email'
+                        className='font-medium text-sm block mb-2'
+                      >
+                        Email Address
+                      </label>
+                      <input
+                        id='email'
+                        type='email'
+                        placeholder='Enter Your Email'
+                        className='text-gray-500 border border-gray-300 focus:ring-0 focus:outline-none focus:border-primary text-sm rounded-lg py-2.5 px-4 w-full'
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor='phone'
+                        className='font-medium text-sm block mb-2'
+                      >
+                        Phone Number
+                      </label>
+                      <input
+                        id='phone'
+                        type='text'
+                        placeholder='Enter Your Phone'
+                        className='text-gray-500 border border-gray-300 focus:ring-0 focus:outline-none focus:border-primary text-sm rounded-lg py-2.5 px-4 w-full'
+                      />
+                    </div>
+                    <button
+                      type='submit'
+                      className='w-full font-siliguri px-6 py-2.5 rounded-lg text-sm font-bold tracking-wide bg-primary text-white cursor-pointer'
+                    >
+                      রেজিস্ট্রেশন করুন
+                    </button>
+                  </form>
+                </div>
+              </div>
+              <div className='mt-6'>
+                <div className='lg:px-6 p-8 bg-[#2D0B70] lg:pb-0 lg:p-12 relative rounded-xl h-full  bg-cover gap-3'>
+                  <div className='max-w-full relative z-10'>
+                    <h3 className='font-bold text-white leading-10 text-center text-xl md:text-3xl mb-4 font-siliguri'>
+                      পেইড কোর্স বা যেকোনো প্রশ্ন বা সাজেশন প্রয়োজন হলে কল
+                      করুন।
+                    </h3>
+                    <p className='text-base text-white/80 mb-6'>
+                      <span className='flex items-center justify-center ml-2 cursor-pointer text-green'>
+                        <svg
+                          stroke='currentColor'
+                          fill='currentColor'
+                          strokeWidth='0'
+                          viewBox='0 0 512 512'
+                          height='1em'
+                          width='1em'
+                          xmlns='http://www.w3.org/2000/svg'
+                        >
+                          <path d='M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z'></path>
+                        </svg>
+                        <span className='ml-2 font-siliguri font-medium'>
+                          ফোন করুন (0179-944-6655)
+                        </span>
+                      </span>
+                    </p>
+                  </div>
+                  <div>
+                    <img
+                      src='/image/icons/bn-3.svg'
+                      className='hidden lg:block relative z-10'
+                      alt='benefit illustration'
+                    />
+                  </div>
+                  <img
+                    src='/image/icons/blur-shape.png'
+                    alt='blur shape'
+                    className='h-full w-full -z-0 absolute top-0 right-0'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SeminarDetails;
--- app/seminars/page.js
+++ app/seminars/page.js
@@ -1,5 +1,46 @@
 import PageBanner from '@/components/PageBanner/PageBanner';
+import Link from 'next/link';
 import { BsFillAlarmFill } from 'react-icons/bs';
+import { FaPersonWalkingArrowLoopLeft } from 'react-icons/fa6';
+
+export const Zoom = (props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='1em'
+    height='1em'
+    preserveAspectRatio='xMidYMid'
+    viewBox='0 0 256 256'
+    {...props}
+  >
+    <defs>
+      <linearGradient
+        id='zoom__a'
+        x1='23.666%'
+        x2='76.334%'
+        y1='95.6118%'
+        y2='4.3882%'
+      >
+        <stop offset='.00006%' stopColor='#0845BF' />
+        <stop offset='19.11%' stopColor='#0950DE' />
+        <stop offset='38.23%' stopColor='#0B59F6' />
+        <stop offset='50%' stopColor='#0B5CFF' />
+        <stop offset='67.32%' stopColor='#0E5EFE' />
+        <stop offset='77.74%' stopColor='#1665FC' />
+        <stop offset='86.33%' stopColor='#246FF9' />
+        <stop offset='93.88%' stopColor='#387FF4' />
+        <stop offset='100%' stopColor='#4F90EE' />
+      </linearGradient>
+    </defs>
+    <path
+      fill='url(#zoom__a)'
+      d='M256 128c0 13.568-1.024 27.136-3.328 40.192-6.912 43.264-41.216 77.568-84.48 84.48C155.136 254.976 141.568 256 128 256c-13.568 0-27.136-1.024-40.192-3.328-43.264-6.912-77.568-41.216-84.48-84.48C1.024 155.136 0 141.568 0 128c0-13.568 1.024-27.136 3.328-40.192 6.912-43.264 41.216-77.568 84.48-84.48C100.864 1.024 114.432 0 128 0c13.568 0 27.136 1.024 40.192 3.328 43.264 6.912 77.568 41.216 84.48 84.48C254.976 100.864 256 114.432 256 128Z'
+    />
+    <path
+      fill='#FFF'
+      d='M204.032 207.872H75.008c-8.448 0-16.64-4.608-20.48-12.032-4.608-8.704-2.816-19.2 4.096-26.112l89.856-89.856H83.968c-17.664 0-32-14.336-32-32h118.784c8.448 0 16.64 4.608 20.48 12.032 4.608 8.704 2.816 19.2-4.096 26.112l-89.6 90.112h74.496c17.664 0 32 14.08 32 31.744Z'
+    />
+  </svg>
+);
 
 const Seminars = () => {
   return (
@@ -10,61 +51,60 @@ const Seminars = () => {
           <div className='grid grid-cols-1 gap-5 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2'>
             <div class='project' data-filter='design'>
               <div class='group rounded-[20px] border-2 border-red-300 bg-white px-4 py-6 transition hover:border-primary hover:bg-primary/10 sm:p-[30px]'>
-                <h6 class='mb-4 text-base font-black uppercase text-secondary'>
-                  30 july, 2025
-                </h6>
-                <h4 class='mb-8 text-xl font-extrabold capitalize text-black  sm:text-1xl'>
-                  <a href='#'>Post Graduate Diploma in Cyber Security</a>
-                </h4>
-                <div class='flex items-center justify-between'>
-                  <div className='flex items-center gap-5'>
-                    <div class='flex items-center gap-2'>
-                      <div>
-                        <svg
-                          width='18'
-                          height='20'
-                          viewBox='0 0 18 20'
-                          fill='none'
-                          xmlns='http://www.w3.org/2000/svg'
-                        >
-                          <path
-                            fill-rule='evenodd'
-                            clip-rule='evenodd'
-                            d='M0 8.45529C0 3.77836 3.8292 0 8.53659 0C13.244 0 17.0732 3.77836 17.0732 8.45529C17.0732 11.3641 15.3769 14.235 13.5791 16.3248C12.6693 17.3823 11.7037 18.2751 10.852 18.9101C10.4268 19.227 10.0181 19.4886 9.64923 19.6744C9.30343 19.8485 8.90853 20 8.53659 20C8.16464 20 7.76974 19.8485 7.42394 19.6744C7.05506 19.4886 6.6464 19.227 6.22121 18.9101C5.36948 18.2751 4.40386 17.3823 3.49409 16.3248C1.69631 14.235 0 11.3641 0 8.45529ZM8.53661 10.9748C6.92017 10.9748 5.60978 9.66437 5.60978 8.04792C5.60978 6.43148 6.92017 5.12109 8.53661 5.12109C10.1531 5.12109 11.4634 6.43148 11.4634 8.04792C11.4634 9.66437 10.1531 10.9748 8.53661 10.9748Z'
-                            fill='#04a9ff'
-                          ></path>
-                        </svg>
+                <div>
+                  <h6 class='mb-4 text-base font-black uppercase text-secondary'>
+                    30 july, 2025
+                  </h6>
+                  <h4 class='mb-8 text-xl font-extrabold capitalize text-black  sm:text-1xl'>
+                    <Link href='/seminars/1'>
+                      Post Graduate Diploma in Cyber Security
+                    </Link>
+                  </h4>
+                  <div class='flex items-center justify-between'>
+                    <div className='flex items-center gap-5'>
+                      <div class='flex items-center gap-2'>
+                        <div>
+                          <Zoom />
+                        </div>
+                        <div class='font-bold text-black capitalize  sm:text-base'>
+                          Online
+                        </div>
                       </div>
-                      <div class='font-bold text-black  sm:text-base'>
-                        offline
+                      <div class='flex items-center gap-2'>
+                        <div>
+                          <BsFillAlarmFill className='text-primary' />
+                        </div>
+                        <div class='font-bold text-black capitalize  sm:text-base'>
+                          10:00 PM
+                        </div>
+                      </div>
+                      <div class='flex items-center gap-2'>
+                        <div>
+                          <FaPersonWalkingArrowLoopLeft className='text-primary' />
+                        </div>
+                        <div class='font-bold text-black capitalize  sm:text-base'>
+                          10 Day left
+                        </div>
                       </div>
                     </div>
-                    <div class='flex items-center gap-2'>
-                      <div>
-                        <BsFillAlarmFill className='text-primary' />
-                      </div>
-                      <div class='font-bold text-black  sm:text-base'>
-                        10:00 PM
-                      </div>
-                    </div>
-                  </div>
-                  <a
-                    href='javascript:'
-                    class='flex h-[45px] w-[45px] items-center justify-center rounded-full bg-[#F3F4F6] text-black transition hover:bg-primary hover:text-white rtl:rotate-180'
-                  >
-                    <svg
-                      width='20'
-                      height='20'
-                      viewBox='0 0 20 20'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
+                    <Link
+                      href='/seminars/1'
+                      class='flex h-[45px] w-[45px] items-center justify-center rounded-full bg-[#F3F4F6] text-black transition hover:bg-primary hover:text-white rtl:rotate-180'
                     >
-                      <path
-                        d='M9.41083 14.4102L10.5892 15.5885L16.1783 9.99932L10.5892 4.41016L9.41083 5.58849L12.9883 9.16599H5V10.8327H12.9883L9.41083 14.4102Z'
-                        fill='currentColor'
-                      ></path>
-                    </svg>
-                  </a>
+                      <svg
+                        width='20'
+                        height='20'
+                        viewBox='0 0 20 20'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          d='M9.41083 14.4102L10.5892 15.5885L16.1783 9.99932L10.5892 4.41016L9.41083 5.58849L12.9883 9.16599H5V10.8327H12.9883L9.41083 14.4102Z'
+                          fill='currentColor'
+                        ></path>
+                      </svg>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
